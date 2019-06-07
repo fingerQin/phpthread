21d81d6ffdcc1c8be51cae8039de1ec4d5bf222e
fingerQin <753814253@qq.com>
2019-06-07 14:26:21 +0800

    Update start.php
    示例增加新特性运用。

--- start.php
+++ start.php
@@ -9,37 +9,29 @@ include_once('Thread.php');
 
 class Start extends Thread
 {
-    // 同时运行的进程数.
-    const PROGRESS_NUM = 10;
-
     /**
      * Process.
-     *
-     * (1) 简单语句：空语句（就一个；号），return,break,continue,throw, goto,global,static,unset,echo, 内置的HTML文本，分号结束的表达式等均算一个语句。
-     * (2) 复合语句：完整的if/elseif,while,do...while,for,foreach,switch,try...catch等算一个语句。
-     * (3) 语句块：{} 括出来的语句块。
-     * (4) 最后特别的：declare块本身也算一个语句(按道理declare块也算是复合语句，但此处特意将其独立出来)。
-     * 所有的statement, function_declare_statement, class_declare_statement就构成了所谓的低级语句(low-level statement)。
      * 
-     * -- for 循环在时间 declare(ticks = 1) 情况下只算一个语句。所以，此时会失效。
-     * -- 针对这种复合语句以及语句块，所以，我们需要使用 pcntl_signal_dispatch() 告诉 PHP，我们触发了一个信号。
-     * -- 在 run 中编写的方法请一定要确定是事务型的。要么成功要么失败。要处于好失败情况下的数据处理。
-     * -- 已经将 pcntl_signal_dispatch() 写入 isTimeout();
-     * 
-     * @param  int  $threadNum  进程数量。
-     * @param  int  $num        子进程编号。
+     * @param  int  $threadNum     进程数量。
+     * @param  int  $num           子进程编号。
+     * @param  int  $startTimeTsp  子进程启动时间戳。
      * 
      * @return void
      */
-    public function run($threadNum, $num)
+    public function run($threadNum, $num, $startTimeTsp)
     {
+        $startDate = date('Y-m-d H:i:s', $startTimeTsp);
         for (;;) {
-            sleep(1);
+            $pid = posix_getpid();
+            file_put_contents('log', "启动时间：{$startDate},主进程PID:{$this->masterPid},子进程PID：{$pid},数量:{$threadNum}，编号：{$num}\n", FILE_APPEND);
+            sleep(3);
+            $this->isExit($startTimeTsp); // 循环操作时间调用。
         }
     }
 }
 
 // 执行多线程业务处理.
-$objThread = Start::getInstance(Start::PROGRESS_NUM);
-$objThread->setChildOverNewCreate(false);
+$objThread = Start::getInstance(2); // 2 个子进程。
+$objThread->setChildOverNewCreate(true);
+$objThread->setRunDurationExit(10);
 $objThread->start();
